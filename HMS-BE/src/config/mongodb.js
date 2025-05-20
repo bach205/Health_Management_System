8c8a3e17bdc1fcaa950a5f575d638c859d13ce5f
theorNg <thaonp411@gmail.com>
2025-05-20 23:37:42 +0700

    feat: connect database & chore: install dotenv, async-exit-hook

--- HMS-BE/src/config/mongodb.js
+++ HMS-BE/src/config/mongodb.js
@@ -1,5 +1,29 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { env } from '~/config/environment'
+import { ServerApiVersion, MongoClient } from 'mongodb'
+
+let testingDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  testingDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+
+export const GET_DB = () => {
+  if (!testingDatabaseInstance) {
+    throw new Error('Database not connected')
+  }
+  return testingDatabaseInstance
+}
--- HMS-BE/src/server.js
+++ HMS-BE/src/server.js
@@ -1,32 +1,36 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+// eslint-disable-next-line no-console
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment'
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+const express = require('express')
 
-const app = express()
+const START_SERVER = () => {
 
-const hostname = 'localhost'
-const port = 8017
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  // ham listCollections -> return promise
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
+
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+//IIFE: Invoke function
+(async () => {
+  try {
+    await CONNECT_DB()
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+})()
+
