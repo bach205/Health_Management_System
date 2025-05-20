57042188ed7524f4d6effbecb08858523539557e
Huy Bach <dhbach13@gmail.com>
2025-05-21 03:35:01 +0700

    create user model and appointment model

---
+++ HMS-BE/src/models/AppointmentModel.js
@@ -0,0 +1,31 @@
+const dayjs = require("dayjs");
+const { Schema, model } = require("mongoose");
+
+const AppointmentSchema = Schema({
+    patientId: { require: true, type: Schema.Types.ObjectId, ref: "user" },
+    doctorId: { type: Schema.Types.ObjectId, ref: "user" },
+    date: String,
+    time: String,
+    dateTime: {
+        type: Date,
+        default: function () {
+            const combinedDateTime = `${this.date} ${this.time}`;
+            return dayjs(combinedDateTime).format(FORMAT_DATE_TIME);
+        },
+    },
+    purpose: String,
+    status: String,
+    serviceType: String,
+    specialty: String,
+    counter: Number,
+    isExamined: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const Appointment = model("appointment", AppointmentSchema);
+
+module.exports = { Appointment };
