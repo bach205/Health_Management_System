d1cc0c56ff59a0b9652dd0673fe660c9d7bbf114
Huy Bach <dhbach13@gmail.com>
2025-06-08 22:06:41 -0400

    feat: add booking-appointment(nurse) api

--- HMS-BE/src/validators/appointment.validator.js
+++ HMS-BE/src/validators/appointment.validator.js
@@ -33,6 +33,46 @@ const bookAppointmentSchema = Joi.object({
   }),
 });
 
+const nurseBookAppointmentSchema = Joi.object({
+  // patient_id: Joi.number().integer().required().messages({
+  //   'number.base': 'ID bệnh nhân phải là số',
+  //   'any.required': 'ID bệnh nhân là bắt buộc'
+  // }),
+  doctor_id: Joi.number().integer().required().messages({
+    'number.base': 'ID bác sĩ phải là số',
+    'any.required': 'ID bác sĩ là bắt buộc'
+  }),
+  clinic_id: Joi.number().integer().required().messages({
+    'number.base': 'ID phòng khám phải là số',
+    'any.required': 'ID phòng khám là bắt buộc'
+  }),
+  appointment_date: Joi.date().greater('now').required().messages({
+    'date.base': 'Ngày hẹn không hợp lệ',
+    'date.greater': 'Ngày hẹn phải lớn hơn ngày hiện tại',
+    'any.required': 'Ngày hẹn là bắt buộc'
+  }),
+  appointment_time: Joi.string()
+    .pattern(/^([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/)
+    .required()
+    .messages({
+      'string.pattern.base': 'Thời gian không đúng định dạng (HH:mm:ss)',
+      'any.required': 'Thời gian hẹn là bắt buộc'
+    }),
+  reason: Joi.string().max(1000).allow("", null).messages({
+    'string.max': 'Lý do không được vượt quá 1000 ký tự'
+  }),
+  note: Joi.string().max(1000).allow("", null).messages({
+    'string.max': 'Ghi chú không được vượt quá 1000 ký tự'
+  }),
+}).unknown(true);
+
+const IsPatientIdValid = Joi.object({
+  patient_id: Joi.number().integer().required().messages({
+    'number.base': 'ID bệnh nhân phải là số',
+    'any.required': 'ID bệnh nhân là bắt buộc'
+  }),
+})
+
 const getAvailableSlotsSchema = Joi.object({
   doctor_id: Joi.number().integer().required().messages({
     'number.base': 'ID bác sĩ phải là số',
@@ -86,4 +126,6 @@ module.exports = {
   confirmAppointmentSchema,
   cancelAppointmentSchema,
   getAppointmentDetailSchema,
+  nurseBookAppointmentSchema,
+  IsPatientIdValid,
 };
