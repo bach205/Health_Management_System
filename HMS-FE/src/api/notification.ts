3bc48f172539a5c48b5b78d843e7e6ea1a516234
Huy Bach <dhbach13@gmail.com>
2025-07-10 03:39:52 +0700

    Create: create api to push notification from front-end side

--- HMS-FE/src/api/notification.ts
+++ HMS-FE/src/api/notification.ts
@@ -3,6 +3,18 @@ import type { NotificationItem } from '../types/notification.type';
 
 const BASE_URL = "api/v1/notification";
 
+interface NotificationData {
+    message: string;
+    isSeen: boolean;
+    userId: number;
+    navigate_url: string;
+}
+
+export const PushANotification = async (data: NotificationData) => {
+    const response = await axios.post(BASE_URL, data);
+    return response
+};
+
 export const markAllNotificationsAsRead = async () => {
     return await axios.patch(`${BASE_URL}/read_all`);
 };
