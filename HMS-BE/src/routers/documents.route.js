c20edfc8a358a647269fc3f38022c7d2660e8b01
Huy Bach <dhbach13@gmail.com>
2025-07-13 18:27:34 -0400

    Fix:stream file with filename in unicode format to rag server

--- HMS-BE/src/routers/documents.route.js
+++ HMS-BE/src/routers/documents.route.js
@@ -2,12 +2,23 @@ const express = require("express");
 const { authenticate, authorize } = require("../middlewares/auth");
 const DocumentsController = require("../controllers/documents.controller");
 const multer = require("multer");
-const upload = multer();
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        // Ép lại encoding đúng nếu bị lỗi Unicode
+        const originalName = Buffer.from(file.originalname, 'latin1').toString('utf8');
+
+        // Làm sạch tên file (nếu muốn)
+        const safeName = originalName.replace(/\s+/g, '_');
+        file.originalname = safeName;
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 const router = express.Router();
 
-// router.use(authenticate);
-// router.use(authorize(["admin"]));
+router.use(authenticate);
+router.use(authorize("admin"));
 
 router.get("/", DocumentsController.getAllDocuments);
 router.get("/:id", DocumentsController.getDocumentById);
