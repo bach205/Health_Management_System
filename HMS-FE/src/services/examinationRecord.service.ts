a421d1f75975bbec847697f13ee1e1a27fc5878f
thaonp411 <thaonp411@gmail.com>
2025-07-17 12:27:16 +0700

    fix: changing appointment

--- HMS-FE/src/services/examinationRecord.service.ts
+++ HMS-FE/src/services/examinationRecord.service.ts
@@ -6,6 +6,10 @@ export const getAppointmentRecordByAppointmentId = async (appointmentId: number)
     return instance.get(`${BASE_URL_RECORD}/appointment/${appointmentId}`);
 };
 
+export const getPatientExaminationHistory = async (patientId: number) => {
+    return instance.get(`${BASE_URL_RECORD}/patient/${patientId}/history`);
+};
+
 export const getAppointmentOrderByAppointmentId = async (appointmentId: number) => {
     return instance.get(`${BASE_URL_ORDER}/appointment/${appointmentId}/order`);
 };
